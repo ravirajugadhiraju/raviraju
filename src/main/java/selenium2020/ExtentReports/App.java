56d581524ef92a2dc6f686c12177b7bb58e90c55
ravirajugadhriaju <raviraju4@gmail.com>
2020-09-10 12:52:45 +0530

    first commit

---
+++ src/main/java/selenium2020/ExtentReports/App.java
@@ -0,0 +1,13 @@
+package selenium2020.ExtentReports;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
